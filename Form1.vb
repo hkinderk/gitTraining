9bf6bcfb1443e7ee3f28c1c48c83d07dd2b8f7f5
hkinderk <39449249+hkinderk@users.noreply.github.com>
2018-05-20 11:35:46 -0300

    test

--- Form1.vb
+++ Form1.vb
@@ -1,7 +1,7 @@
 ﻿Public Class Form1
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        private void Test2 ()
+        private void Test3 ()
         {
 }
     End Sub
